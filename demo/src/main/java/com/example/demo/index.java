d81b555a95c9e6aa33979738b76f29106381f8f5
Felipe Solís <felipe.solis@usach.cl>
2022-04-19 11:58:34 -0400

    Cambio ruta

--- demo/src/main/java/com/example/demo/index.java
+++ demo/src/main/java/com/example/demo/index.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class index {
 
-    @GetMapping("/")
+    @GetMapping("/mensaje")
     public String HelloWorld() {
         return "Aplicación web simple.";
     }
